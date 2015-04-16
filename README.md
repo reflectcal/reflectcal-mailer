abbfa577b5f66fe2cab0efb10006cd3ac7d7b699
alexeykomov <alexeykofficial@gmail.com>
2015-04-17 00:53:38 +0300

    Intermediate commit.

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
-# reflectcalMailer
+Reflectcal mailer
+================
 
-> Mailer daemon for reflectcal.com
+Daemon for sending e-mail, part of reflectcal.com.
+
+Installation and run
+--------------------
+
+1. Clone this repository locally.
+
+  ```
+  mkdir reflectcal
+  cd reflectcal
+  git clone https://github.com/alexeykomov/reflectcal.git
+  ```
+
+8. Go to [http://localhost:3000/](http://localhost:3000/)
+
+[jdk]: http://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html
+[python]: https://www.python.org/download/releases/2.7/
+[node.js]: http://nodejs.org/download/
+[mongodb]: http://www.mongodb.org/downloads
+[express]: https://github.com/visionmedia/express
+[grunt]: https://github.com/gruntjs/grunt
+[less]: https://github.com/less/less
+[bunyan]: https://github.com/trentm/node-bunyan
+[devconsole]: https://console.developers.google.com/project?authuser=1 
--- src/daemons/mailer.js
+++ src/daemons/mailer.js
@@ -9,6 +9,7 @@
 
 
 import * as appConfig from '../config/appconfig';
+import { db } from '../db/connection.js'
 const log = appConfig.log;
 const SECOND = 1000;
 const MINUTE = SECOND * 60;
@@ -55,8 +56,33 @@ function notificationLoopCallback() {
 
     console.log('Requesting interval from: ', new Date(intervalStart).toISOString(), ' to: ', new Date(intervalStart + MINUTE).toISOString())
 
-
+    getCloseEventsWithPromise({}).then(aEvents => {
+      aEvents.filter(aEvent => {
+        var eventStartTime = aEvent.start;
+        return (aEvent.alerts || []).some(aAlert => {
+          var intervalStart = aNowTime + aAlert.interval;
+          var intervalEnd = intervalStart + 1000 * 60;
+          console.log('intervalStart: ', new Date(intervalStart).toISOString());
+          return 3 == aAlert.type && eventStartTime >= intervalStart &&
+             eventStartTime < intervalEnd;)
+        })
+      })
+    }, log)
   }
   checkTimer = setTimeout(notificationLoopCallback, SECOND);
 }
 
+
+function onMinuteCallback() {
+  db.get('events')
+
+}
+
+
+function getCloseEventsWithPromise(aLookupObject) {
+  return new Promise(function(resolve, reject) {
+    var collection = db.get('events');
+    var findWithPromise = Q.denodeify(collection.find.bind(collection));
+    findWithPromise(aLookupObject, {}).then(resolve, reject);
+  });
+}
